06886f14210a5c184090dd795fd53183ff661973
KHD <nlom0218@naver.com>
2021-03-08 20:22:00 +0900

    day 2
    day 2

--- src/App.js
+++ src/App.js
@@ -1,56 +1,31 @@
 import React from "react";
+// import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "김치",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-  },
-  {
-    id: 2,
-    name: "삼겹살",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-  },
-  {
-    id: 3,
-    name: "비빕밥",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-  },
-  {
-    id: 4,
-    name: "돈까스",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-  },
-  {
-    id: 5,
-    name: "김밥",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-  },
-];
+class App extends React.Component {
+  state = {
+    isLoading: true,
+  };
 
-function Food({ fav, picture }) {
-  return (
-    <div>
-      <div>내가 좋아하는 음식: {fav}</div>
-      <img src={picture} alt={fav} />
-    </div>
-  );
-}
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
+  }
 
-function App() {
-  return (
-    <div>
-      <h1>내가 좋아하는 음식</h1>
-      {foodILike.map((item) => {
-        return <Food id={item.id} fav={item.name} picture={item.image} />;
-      })}
-    </div>
-  );
+  componentDidUpdate() {
+    setTimeout(() => {
+      console.log("I'm update!!");
+    }, 3000);
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div>
+        <h1>{isLoading ? "Loading..." : "We are ready"}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
